c4cab86f2ae99824034c86e2a233ac5e26df81e3
Patrick <ailapatrick@yahoo.com>
2023-03-28 10:20:06 +0300

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import About from "./About"
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2023</p>
+      <a href="/about">About</a>
+    </footer>
+  )
+}
+
+export default Footer
